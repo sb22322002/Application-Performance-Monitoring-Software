567cfab6cf5a4d1a82ba440ea6cf4913210554e1
Alex <81817203+arogoff@users.noreply.github.com>
2021-10-18 15:53:41 -0400

    Updated APMTool.sh
    - Figured out the commands we will be using to collect the data. 
    - Renamed killAll function to cleanup
    - Set up the trap (When the script gets interrupted/exited, it will run the cleanup function
    - Prepared sysMetrics for the commands and writing to specific files
    - Prepared procMetrics for the commands and writing to specific files

--- APMTool.sh
+++ APMTool.sh
@@ -31,27 +31,68 @@ spawnAll(){
 
 # required function to collect system level metrics
 sysMetrics(){
+	# Command to collect RX and TX data rates
+	# Need to separate it out and only get the ens33 interface (Fields 7 & 9)
+	ifstat -d 1 # "-d 1" sets to 1 second
+	
+	RX_RATE=1
+	TX_RATE=1
 
+	# Command to collect hard disk utilization
+	# Need to separate it out and only get with the "/" mount (can do -T / i think)
+	df -m # "-m" displays everything in Megabytes
+	
+	DISK_WRITES=1
+	AVAIL_DISK_CAPACITY=1
+	
+	seconds=0
+
+	echo "${seconds},${RX_RATE},${TX_RATE},${DISK_WRITES},${AVAIL_DISK_CAP}" >> system_metrics.csv
 }
 
 # required function to collect process level metrics 
 procMetrics(){
+	# Command to collect %CPU and %memory
+	# Need to separate it out and only get it for the APM1-6
+	ps -o %cpu,%mem,cmd #cmd displays for APM
+
+	seconds=0
+	
+	# For loop to write each process's metrics in its own file
+	for i in 1..6
+	do
+		CPU=0
+		MEMORY=0
+		echo "${seconds},${CPU},${MEMORY}" >> APM$i_metrics.csv
+	done
 
 }
 
-killAll(){
+cleanup(){
 	kill $(ps | grep APM1 | awk '{print $1}')
 	kill $(ps | grep APM2 | awk '{print $1}')
 	kill $(ps | grep APM3 | awk '{print $1}')
 	kill $(ps | grep APM4 | awk '{print $1}')
 	kill $(ps | grep APM5 | awk '{print $1}')
 	kill $(ps | grep APM6 | awk '{print $1}')
+
+	echo "done"
 }
 
+# Trap - When the script ends it calls the cleanup function
+# Note: This must stay on top!
+trap cleanup EXIT
+
 spawnAll
-## testing purposes... otherwise it will sometimes kill before the apps even run
-sleep 2
-killAll
+
+# While loop to run through each process need to do exit trap function
+while [[ true ]]
+do
+	sysMetrics
+	procMetrics
+	sleep 5
+	
+done
 
 ####################################
 # right now my output looks like this:
@@ -64,4 +105,5 @@ killAll
 # 	./APMTool.sh: line 45: 17047 Terminated              ./APM5 $ip
 # 	[sb2232 ~/project1]$
 ####################################
+
 
