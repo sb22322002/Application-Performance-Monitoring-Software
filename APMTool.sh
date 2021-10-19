3b5d732e7a3a62dc2e6d634cd2c81b8d0f3728d3
sb22322002 <73617016+sb22322002@users.noreply.github.com>
2021-10-19 00:20:20 -0400

    Update APMTool.sh

--- APMTool.sh
+++ APMTool.sh
@@ -16,8 +16,6 @@ spawnAll(){
 	chmod 755 APM6
 
 	# store ip address then run applications
-	## not sure if this is the right way to get ip address but it takes the
-	## ip4 of inet
 	ip=$(ifconfig | head -2 | tail -1 | awk '{print $2}')
 	
 	# run apps in the backround with &
@@ -27,56 +25,71 @@ spawnAll(){
 	./APM4 $ip &
 	./APM5 $ip &
 	./APM6 $ip &
+
+	# initialize variables to keep track of time
+	#############################################################
+	# time won't display first time
+	#############################################################
+	declare -i time="0"
+	
+	# create new files system_metrics.csv and <proc_name>_metrics.csv
+	echo -n "" > system_metrics.csv
+	for i in {1..6}
+	do
+		echo -n "" > "APM${i}_metrics.csv"
+	done
 }
 
 # required function to collect system level metrics
 sysMetrics(){
 	# Command to collect RX and TX data rates
 	# Need to separate it out and only get the ens33 interface (Fields 7 & 9)
-	ifstat -d 1 # "-d 1" sets to 1 second
+	# "-d 1" sets to 1 second
+	ifstat -d 1 > /dev/null 2>&1
 	
-	RX_RATE=1
-	TX_RATE=1
+	RX_RATE=$(ifstat| grep ens | awk '{print $7}')
+	TX_RATE=$(ifstat| grep ens | awk '{print $9}')
 
 	# Command to collect hard disk utilization
 	# Need to separate it out and only get with the "/" mount (can do -T / i think)
-	df -m # "-m" displays everything in Megabytes
+	## df -m # "-m" displays everything in Megabytes
 	
-	DISK_WRITES=1
-	AVAIL_DISK_CAPACITY=1
-	
-	seconds=0
+	DISK_WRITES=$(iostat | grep sda | awk '{print $4}')
+	AVAIL_DISK_CAP=$(df -mT / | tail -1 | awk '{print $5}')
 
-	echo "${seconds},${RX_RATE},${TX_RATE},${DISK_WRITES},${AVAIL_DISK_CAP}" >> system_metrics.csv
+	echo "${1},${RX_RATE},${TX_RATE},${DISK_WRITES},${AVAIL_DISK_CAP}" >> system_metrics.csv
 }
 
 # required function to collect process level metrics 
 procMetrics(){
 	# Command to collect %CPU and %memory
 	# Need to separate it out and only get it for the APM1-6
-	ps -o %cpu,%mem,cmd #cmd displays for APM
 
-	seconds=0
-	
+	## ps -o %cpu,%mem,cmd # cmd displays for APM
+
 	# For loop to write each process's metrics in its own file
-	for i in 1..6
+	for i in {1..6}
 	do
-		CPU=0
-		MEMORY=0
-		echo "${seconds},${CPU},${MEMORY}" >> APM$i_metrics.csv
+		CPU=$(ps -o %cpu,cmd | grep "APM$i" | head -1 | awk '{print $1}')
+		MEMORY=$(ps -o %mem,cmd | grep "APM$i" | head -1 | awk '{print $1}')
+		echo "${1},${CPU},${MEMORY}" >> "APM${i}_metrics.csv"
 	done
-
+	
+	
 }
 
 cleanup(){
-	kill $(ps | grep APM1 | awk '{print $1}')
-	kill $(ps | grep APM2 | awk '{print $1}')
-	kill $(ps | grep APM3 | awk '{print $1}')
-	kill $(ps | grep APM4 | awk '{print $1}')
-	kill $(ps | grep APM5 | awk '{print $1}')
-	kill $(ps | grep APM6 | awk '{print $1}')
+	#############################################################
+	# trying to get rid of the messages at the end when killing jobs
+	#############################################################
+	kill $(ps | grep APM1 | awk '{print $1}') > /dev/null 2>&1
+	kill $(ps | grep APM2 | awk '{print $1}') > /dev/null 2>&1
+	kill $(ps | grep APM3 | awk '{print $1}') > /dev/null 2>&1
+	kill $(ps | grep APM4 | awk '{print $1}') > /dev/null 2>&1
+	kill $(ps | grep APM5 | awk '{print $1}') > /dev/null 2>&1
+	kill $(ps | grep APM6 | awk '{print $1}') > /dev/null 2>&1
 
-	echo "done"
+	echo "Complete"
 }
 
 # Trap - When the script ends it calls the cleanup function
@@ -88,22 +101,11 @@ spawnAll
 # While loop to run through each process need to do exit trap function
 while [[ true ]]
 do
-	sysMetrics
-	procMetrics
+	sysMetrics "$time"
+	procMetrics "$time"
+
+	echo "Metrics collected - TIME: $time seconds"
+
 	sleep 5
-	
+	time=$(($time+5))
 done
-
-####################################
-# right now my output looks like this:
-#
-# 	[sb2232 ~/project1]$ ./APMTool.sh
-# 	./APMTool.sh: line 36: 17043 Terminated              ./APM1 $ip
-# 	./APMTool.sh: line 37: 17044 Terminated              ./APM2 $ip
-# 	./APMTool.sh: line 38: 17045 Terminated              ./APM3 $ip
-# 	./APMTool.sh: line 39: 17046 Terminated              ./APM4 $ip
-# 	./APMTool.sh: line 45: 17047 Terminated              ./APM5 $ip
-# 	[sb2232 ~/project1]$
-####################################
-
-
